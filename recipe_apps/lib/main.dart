859c3550d208b12958059aabbc6e19feb83b7ba8
Amir Mazlan <46234864+Zer0Ner0@users.noreply.github.com>
2025-07-28 17:13:12 +0800

    Convert main app widget to StatefulWidget
    Enables future state management within the main app by converting it from a stateless to a stateful widget. Prepares for features that may require internal state without affecting current UI or functionality.

--- recipe_apps/lib/main.dart
+++ recipe_apps/lib/main.dart
@@ -5,14 +5,19 @@ void main() {
   runApp(const MyRecipeApp());
 }
 
-class MyRecipeApp extends StatelessWidget {
+class MyRecipeApp extends StatefulWidget {
   const MyRecipeApp({super.key});
 
+  @override
+  State<MyRecipeApp> createState() => _MyRecipeAppState();
+}
+
+class _MyRecipeAppState extends State<MyRecipeApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+        // scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
       ),
       home: const SplashScreen(), // ✅ Directly load SplashScreen
     );
